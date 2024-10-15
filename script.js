9c019dbe82d9c4d8265c29a25f596e0d7ecee181
nagarajsv <117421190+nagarajsv@users.noreply.github.com>
2024-10-14 19:20:33 -0500

    Personal website implemented

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // script.js
 
 // Alert message when the page loads
-window.onload = function() {
-  alert("Welcome to my first web page!");
-};
 
 // Function to change the text when button is clicked
 function changeText() {
